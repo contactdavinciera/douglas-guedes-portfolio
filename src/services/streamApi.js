9f26e9b25d1d5cef53c09555066baef0fbb3e529
contactdavinciera <236133761+contactdavinciera@users.noreply.github.com>
2025-10-07 09:17:37 -0400

    Corrigir maxDurationSeconds no frontend para garantir limite de 5h

--- src/services/streamApi.js
+++ src/services/streamApi.js
@@ -32,7 +32,7 @@ class StreamApiService {
         body: JSON.stringify({
           uploadLength: file.size,
           uploadMetadata: metadataString,
-          maxDurationSeconds,
+          maxDurationSeconds: Math.min(maxDurationSeconds, 18000), // garante limite de 5h,
           allowedOrigins: [this.frontendOrigin] // Passar o origin do frontend para o backend
         })
       });
