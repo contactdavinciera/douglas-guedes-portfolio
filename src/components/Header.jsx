bac172510d96c69fdc35a9d48a39123fb2923d21
contactdavinciera <contact@davinciera.com>
2025-10-07 22:44:56 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Menu, X } from 'lucide-react'
-import { Button } from "../../components/ui/button"
+import { Button } from "../components/ui/button"
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
